9368bc991acf5088f0458c665f35289b20bbea4a
LuminousV Windows <farelreyhan6@gmail.com>
2025-04-26 22:13:58 +0700

    Creating Folder Project and make a login page

---
+++ database/migrations/2025_04_26_133458_add_employee_data_into_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string("phone")->nullable();
+            $table->string("address")->nullable();
+            $table->string("joining_date")->nullable();
+            $table->string("photo")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("phone"); // Untuk menghapus column
+            $table->dropColumn("address"); // Untuk menghapus column
+            $table->dropColumn("joining_date"); // Untuk menghapus column
+            $table->dropColumn("photo"); // Untuk menghapus column
+        });
+    }
+};
